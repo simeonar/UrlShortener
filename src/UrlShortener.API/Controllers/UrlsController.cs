e523f8357e2fda2694b9194bc591688054f233d4
Semen Arshyn <semenarshyn@gmail.com>
2025-07-11 11:56:45 +0200

    Add UrlsController with POST /api/shorten endpoint

---
+++ src/UrlShortener.API/Controllers/UrlsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UrlShortener.Core.Entities;
+using UrlShortener.Core.Services;
+
+namespace UrlShortener.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UrlsController : ControllerBase
+    {
+        private readonly IShortCodeGenerator _shortCodeGenerator;
+        // TODO: Inject repository/service for persistence
+
+        public UrlsController(IShortCodeGenerator shortCodeGenerator)
+        {
+            _shortCodeGenerator = shortCodeGenerator;
+        }
+
+        /// <summary>
+        /// Creates a new shortened URL.
+        /// </summary>
+        [HttpPost("shorten")]
+        public async Task<IActionResult> Shorten([FromBody] ShortenUrlRequest request)
+        {
+            // TODO: Add validation, persistence, etc.
+            var entity = new ShortenedUrl
+            {
+                Id = Guid.NewGuid(),
+                OriginalUrl = request.OriginalUrl,
+                CreatedAt = DateTime.UtcNow
+            };
+            var code = await _shortCodeGenerator.GenerateShortCodeAsync(entity);
+            entity.ShortCode = code;
+            // TODO: Save entity to DB
+            return Ok(new { shortCode = code });
+        }
+    }
+
+    public class ShortenUrlRequest
+    {
+        public string OriginalUrl { get; set; } = string.Empty;
+        public string? CustomAlias { get; set; }
+    }
+}
