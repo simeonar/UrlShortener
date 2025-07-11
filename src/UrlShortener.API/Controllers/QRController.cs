c16aa51fa2249bea70611a9a76829ab206326525
Semen Arshyn <semenarshyn@gmail.com>
2025-07-11 13:39:43 +0200

    Fix project structure, update dependencies, clean up .gitignore and remove IDE files from index

--- src/UrlShortener.API/Controllers/QRController.cs
+++ src/UrlShortener.API/Controllers/QRController.cs
@@ -1,23 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
-using QRCoder;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Threading.Tasks;
-using UrlShortener.Core.Services;
-
 namespace UrlShortener.API.Controllers
 {
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using QRCoder;
+    using UrlShortener.Core.Services;
+
+    /// <summary>
+    /// Controller for generating and caching QR codes for short URLs.
+    /// </summary>
     [ApiController]
     [Route("api/qr")]
     public class QRController : ControllerBase
     {
-        private readonly IQRCodeCache _qrCodeCache;
+        private readonly IQRCodeCache qrCodeCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QRController"/> class.
+        /// </summary>
+        /// <param name="qrCodeCache">QR code cache service.</param>
         public QRController(IQRCodeCache qrCodeCache)
         {
-            _qrCodeCache = qrCodeCache;
+            this.qrCodeCache = qrCodeCache;
         }
 
+        /// <summary>
+        /// Returns a QR code for the given short code.
+        /// </summary>
+        /// <param name="shortCode">Short URL code.</param>
+        /// <param name="size">Size of the QR code.</param>
+        /// <param name="format">Format: png or svg.</param>
+        /// <returns>QR code image file.</returns>
         [HttpGet("{shortCode}")]
         public async Task<IActionResult> GetQr(string shortCode, int size = 200, string format = "png")
         {
@@ -25,29 +40,28 @@ namespace UrlShortener.API.Controllers
             var cacheKey = $"{shortCode}:{size}:{format.ToLower()}";
             if (format.ToLower() == "svg")
             {
-                var svgBytes = await _qrCodeCache.GetOrAddAsync(cacheKey, async () =>
+                var svgBytes = await this.qrCodeCache.GetOrAddAsync(cacheKey, async () =>
                 {
+                    await Task.Yield();
                     using var qrGenerator = new QRCodeGenerator();
                     using var qrData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
                     var svgQr = new SvgQRCode(qrData);
                     var svg = svgQr.GetGraphic(size);
                     return System.Text.Encoding.UTF8.GetBytes(svg);
                 });
-                return File(svgBytes, "image/svg+xml");
+                return this.File(svgBytes, "image/svg+xml");
             }
             else
             {
-                var pngBytes = await _qrCodeCache.GetOrAddAsync(cacheKey, async () =>
+                var pngBytes = await this.qrCodeCache.GetOrAddAsync(cacheKey, async () =>
                 {
+                    await Task.Yield();
                     using var qrGenerator = new QRCodeGenerator();
                     using var qrData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
-                    using var qrCode = new QRCode(qrData);
-                    using var bitmap = qrCode.GetGraphic(size / 10 > 0 ? size / 10 : 20);
-                    using var ms = new MemoryStream();
-                    bitmap.Save(ms, ImageFormat.Png);
-                    return ms.ToArray();
+                    using var qrCode = new BitmapByteQRCode(qrData);
+                    return qrCode.GetGraphic(size / 10 > 0 ? size / 10 : 20);
                 });
-                return File(pngBytes, "image/png");
+                return this.File(pngBytes, "image/png");
             }
         }
     }
