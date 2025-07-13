4ab7185b6c1490b7763fa3fafbfefebaba3ba886
Semen Arshyn <semenarshyn@gmail.com>
2025-07-13 08:57:19 +0200

    fix(web): enable Angular router navigation in navbar and import required modules

--- UrlShortener.Web/src/app/app.component.ts
+++ UrlShortener.Web/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
