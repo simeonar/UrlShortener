a50759a531e812301f0e3a0a8b32bcd47814cb80
Semen Arshyn <semenarshyn@gmail.com>
2025-07-14 17:35:33 +0200

    chore(ssr): ensure fetch API is enabled for HttpClient in main.ts (client entrypoint)

--- UrlShortener.Web/src/main.ts
+++ UrlShortener.Web/src/main.ts
@@ -2,13 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app-routing.module';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideAnimations(),
   ]
 }).catch(err => console.error(err));
